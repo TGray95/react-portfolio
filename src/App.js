3c5138d4e6423cf2612fce4b4897319722afbf7b
TGray <gray.tony95@gmail.com>
2023-02-11 19:09:06 -0500

    updated with correct react version

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
